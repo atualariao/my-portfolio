ae85532ca9f68a4b239304021349bbace708aa63
Adriane Troy Urtiz Alariao <adrianetroyalariao@gmail.com>
2026-01-04 22:31:30 +0800

    - fix footer and add appropriate links; add stackoverflow, indeed, and upwork links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   const socialLinks = [
     {
       name: "GitHub",
-      href: "https://github.com/yourusername",
+      href: "https://github.com/atualariao",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,7 @@ export default function Footer() {
     },
     {
       name: "LinkedIn",
-      href: "https://linkedin.com/in/yourusername",
+      href: "https://linkedin.com/in/adriane-troy-alariao-363a8123b/",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ export default function Footer() {
     },
     {
       name: "Email",
-      href: "mailto:your.email@example.com",
+      href: "mailto:adrianetroyalariao@gmail.com",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -53,6 +53,51 @@ export default function Footer() {
         >
           <rect width="20" height="16" x="2" y="4" rx="2" />
           <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+        </svg>
+      ),
+    },
+    {
+      name: "Upwork",
+      href: "https://www.upwork.com/freelancers/~01fa2a8422c579a09b?mp_source=share",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M18.561 13.158c-1.102 0-2.135-.467-3.074-1.227l.228-1.076.008-.042c.207-1.143.849-3.06 2.839-3.06 1.492 0 2.703 1.212 2.703 2.703-.001 1.489-1.212 2.702-2.704 2.702zm0-8.14c-2.539 0-4.51 1.649-5.31 4.366-1.22-1.834-2.148-4.036-2.687-5.892H7.828v7.112c-.002 1.406-1.141 2.546-2.547 2.548-1.405-.002-2.543-1.143-2.545-2.548V3.492H0v7.112c0 2.914 2.37 5.303 5.281 5.303 2.913 0 5.283-2.389 5.283-5.303v-1.19c.529 1.107 1.182 2.229 1.974 3.221l-1.673 7.873h2.797l1.213-5.71c1.063.679 2.285 1.109 3.686 1.109 3 0 5.439-2.452 5.439-5.45 0-3-2.439-5.439-5.439-5.439z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Indeed",
+      href: "https://profile.indeed.com/p/adrianetroya-z47yg1r",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M11.566 21.5v-8.535a1.02 1.02 0 00-1.018-1.018.988.988 0 00-.958 1.018v8.535a.958.958 0 001.976 0zm1.283-13.034c-.382 1.409-1.409 2.436-2.877 3.03V9.431C11.744 8.862 13.33 7.476 14 5.69c.67-1.887.25-4.09-1.164-5.28A4.509 4.509 0 0010.49 0C8.744 0 7.15.61 6.063 1.677c-2.14 2.1-2.14 5.5 0 7.6.67.67 1.468 1.13 2.356 1.468v2.436c-2.616-.67-4.87-2.316-6.16-4.582C.848 6.063 0 3.327 0 .33v-.33h2.436v.33c0 2.436.67 4.752 1.857 6.608 1.13 1.677 2.676 2.975 4.503 3.625V8.225c-.61-.25-1.2-.61-1.677-1.13-1.409-1.408-1.409-3.684 0-5.092.728-.729 1.677-1.13 2.676-1.13a3.66 3.66 0 012.676 1.13c.909.908 1.18 2.316.67 3.463z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Stack Overflow",
+      href: "https://stackoverflow.com/users/20227059/troyurtiz",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M18.986 21.865v-6.404h2.134V24H1.844v-8.539h2.13v6.404h15.012zM6.111 19.731H16.85v-2.137H6.111v2.137zm.259-4.852l10.48 2.189.451-2.07-10.478-2.187-.453 2.068zm1.359-5.056l9.705 4.53.903-1.95-9.706-4.53-.902 1.936v.014zm2.715-4.785l8.217 6.855 1.359-1.62-8.216-6.853-1.35 1.617-.01.001zM15.751 0l-1.746 1.294 6.405 8.604 1.746-1.294L15.749 0h.002z" />
         </svg>
       ),
     },
